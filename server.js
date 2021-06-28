2ce35ce78ae130fea381963e5b47ca7cd4ea3790
David Figueroa <figueroada@vcu.edu>
2021-06-28 01:39:50 -0400

    added session store

--- server.js
+++ server.js
@@ -2,13 +2,24 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const helpers = require('./utils/dates');
 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+const sess = {
+  secret: 'NBA techno',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 app.use(session(sess));
 
 const hbs = exphbs.create({ dates });
