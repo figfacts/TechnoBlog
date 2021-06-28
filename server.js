2a6b4b73dad0691de864cc608afe5d137417c532
David Figueroa <figueroada@vcu.edu>
2021-06-28 01:55:23 -0400

    update server'

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const helpers = require('./utils/dates');
+const dates = require('./utils/dates');
 
 
 const app = express();
