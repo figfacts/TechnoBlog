72339498065aa633303a07c00e7b95e86dff53e3
David Figueroa <figueroada@vcu.edu>
2021-06-28 01:32:18 -0400

    added posts routes

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,52 @@
+const router = require('express').Router();
+const { Post } = require('../../models');
+const withAuthentication = require('../../utils/authentication');
+
+router.post('/', withAuthentication, async (req, res) => {
+  const body = req.body;
+
+  try {
+    const newPost = await Post.create({ ...body, userId: req.session.userId });
+    res.json(newPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.put('/:id', withAuthentication, async (req, res) => {
+  try {
+    const [affectedRows] = await Post.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (affectedRows > 0) {
+      res.status(200).end();
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete('/:id', withAuthentication, async (req, res) => {
+  try {
+    const [affectedRows] = Post.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (affectedRows > 0) {
+      res.status(200).end();
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
